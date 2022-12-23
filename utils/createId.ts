c144b06b370eb9e3aa98ef90c0f227cea6fc84ce
Ikram <ikramzulfugar@gmail.com>
2022-12-23 21:04:52 +0400

    deleted unnecesarry files

--- utils/createId.ts
+++
@@ -1,6 +0,0 @@
-export const createId = (provider: string) => {
-    provider = provider.includes('google') ? "google" : "github"
-    const prefix = `_${provider}_`
-
-    return prefix + crypto.randomUUID()
-}
