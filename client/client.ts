4053fefecb32ea581f7df1f2385355361364ed00
Ikram <ikramzulfugar@gmail.com>
2023-03-03 21:56:43 +0400

    update cache when createPost mutation

--- client/client.ts
+++ client/client.ts
@@ -1,5 +1,7 @@
 import { cacheExchange, CacheExchangeOpts } from '@urql/exchange-graphcache'
 import { createClient, dedupExchange, errorExchange, fetchExchange } from 'urql'
+import { PostsDocument } from '../generated/graphql'
+import { Post } from '../types/types'
 
 
 const client = createClient({
@@ -7,7 +9,15 @@ const client = createClient({
     exchanges: [
       dedupExchange,
       cacheExchange({
-        
+        updates: {
+          Mutation: {
+            createPost(parent , args , cache , info) {
+              const posts: Post[] | null = cache.readQuery({ query : PostsDocument })
+              console.log("====== POSTS =======")
+              
+            }
+          }
+        }
       } as CacheExchangeOpts),
       errorExchange({
         onError(error, operation) {
